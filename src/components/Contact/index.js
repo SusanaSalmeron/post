9a55537f63908997f136259b8d19843f076b82c1
SusanaSalmeron <exdream76@gmail.com>
2021-09-09 11:51:58 +0200

    showing errors on contact form & reset form

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -4,6 +4,26 @@ import style from './contact.module.css';
 
 
 
+const validation = values => {
+    let errors = {};
+
+    if (!values.email) {
+        errors.email = 'Este campo no puede estar vacio';
+    } else if (
+        !/^[A-Z0-9._%+-]+@[A-Z0-9·-]+\.[A-Z]{2,}$/i.test(values.email)) {
+        errors.email = 'Email incorrecto';
+    }
+    if (!values.name) {
+        errors.name = 'Introduce un email'
+    } else if (values.name.length <= 3) {
+        errors.name = "El nombre tiene que ser mayor a 3 caracteres"
+    }
+    if (!values.reason) {
+        errors.reason = 'Escribe tu consulta'
+    }
+    return errors;
+}
+
 
 export default function Contact() {
 
@@ -13,37 +33,27 @@ export default function Contact() {
             email: '',
             reason: ''
         }}
-        validate={values => {
-            let errors = {};
-            if (!values.email) {
-                errors.email = 'Required';
-            } else if (
-                !/^[A-Z0-9._%+-]+@[A-Z0-9·-]+\.[A-Z]{2,}$/i.test(values.email)) {
-                errors.email = 'Email incorrecto';
-            }
-            if (!values.name) {
-                errors.fullname = 'Required';
-            }
-            if (!values.reason) {
-                errors.reason = 'Required'
-            }
-            return errors;
-        }}
-        onSubmit={(values, { setSubmitting }) => {
+
+        onSubmit={(values, { setSubmitting, resetForm }) => {
             setTimeout(() => {
                 alert(JSON.stringify(values, null, 2));
                 setSubmitting(false);
             }, 1000);
+            resetForm({ values: '' })
+
 
         }}
+        validate={validation}
     >
+
         {({
             values,
             errors,
             touched,
             handleChange,
             handleSubmit,
-            isSubmitting
+            isSubmitting,
+
         }) => (
             <form onSubmit={handleSubmit}>
                 <h1 className={style.title}>Contacta con nosotros</h1>
@@ -55,8 +65,9 @@ export default function Contact() {
                             value={values.name}
                             placeholder="Escribe tu nombre"
                         />
-                        {errors.name && touched.name}
                     </div>
+                    {errors.name && touched.name && <p className={style.error}>{errors.name}</p>}
+
                     <div className={style.email}>
                         <input
                             name="email"
@@ -64,7 +75,7 @@ export default function Contact() {
                             value={values.email}
                             placeholder="Email" type="email" />
                     </div>
-                    {errors.email && touched.email}
+                    {errors.email && touched.email && <p className={style.error}>{errors.email}</p>}
                     <div className={style.reason}>
                         <textarea
                             className="reason"
@@ -74,12 +85,12 @@ export default function Contact() {
                             placeholder="Escribe tu consulta" type="text"
                             rows="8" cols="50" />
                     </div>
-                    {errors.reason && touched.reason}
+                    {errors.reason && touched.reason && <p className={style.error}>{errors.reason}</p>}
                 </div>
                 <button
                     type="submit"
                     disabled={isSubmitting} >
-                    {isSubmitting ? 'Submitting' : 'Submit'}
+                    {isSubmitting ? 'Enviando' : 'Enviar'}
                 </button>
             </form>
         )}
